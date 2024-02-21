280dad5617970b232b103c59e6b1c68a0b37768c
Cesar Roman <cesardroman18@gmail.com>
2024-02-21 13:35:22 -0600

    have login render on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,13 @@
 import React, { useState } from "react";
 
 import { Theme } from "@twilio-paste/core/theme";
-import { Button } from "@twilio-paste/core/button";
+
+import Login from "./login/page.tsx";
 
 export default function App() {
-  const [number, setNumber] = useState(0);
-
-  function handlePress() {
-    let randomNumber = Math.floor(Math.random() * 10000) % 100;
-    setNumber(randomNumber);
-  }
-
   return (
     <Theme.Provider theme="default">
-      <div>
-        <p>Random number: {number}</p>
-        <Button variant="primary" onClick={handlePress}>
-          Generate a number
-        </Button>
-      </div>
+      <Login />
     </Theme.Provider>
   );
 }
