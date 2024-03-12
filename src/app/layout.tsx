225e883a21dfd290566f3e4469a93b2dd82cdd23
Cesar Roman <cesardroman18@gmail.com>
2024-03-12 16:42:37 -0500

    style: edited layout for wrapper style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className} parent`}>{children}</body>
       </html>
     </ClerkProvider>
   );
