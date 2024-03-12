68f2fe8d0a47205517ec4235461c19ae157db242
Cesar Roman <cesardroman18@gmail.com>
2024-03-12 16:42:02 -0500

    edited app page to render landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import React, { useState } from "react";
-
 import { Theme } from "@twilio-paste/core/theme";
 
-import OnboardingFlow from "./onboarding/page";
-import Login from "./login/page";
+import Landing from "./landing/page";
 
 export default function App() {
-  const [loggedIn, _] = useState<boolean>();
-
   return (
     <Theme.Provider theme="default">
-      {loggedIn ? <OnboardingFlow /> : <Login />}
+      <Landing />
     </Theme.Provider>
   );
 }
