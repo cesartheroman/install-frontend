a66edae8b37bd02a606a0d193799118ab247017d
Cesar Roman <cesardroman18@gmail.com>
2024-04-01 17:12:05 -0500

    feat: implemented skeleton logic to handle skipped onboarding

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,24 @@
 "use client";
 import React from "react";
 
+import { useUser } from "@clerk/nextjs";
+
 import BarChart from "../components/BarChart";
 import DoughnutChart from "../components/DoughnutChart";
 import LineChart from "../components/LineChart";
 import PolarAreaChart from "../components/PolarAreaChart";
 
 const Dashboard = () => {
+  const { user } = useUser();
+  if (!user) return null;
+
+  const skippedOnboarding = user.unsafeMetadata.skippedOnboarding;
+  if (skippedOnboarding) {
+    console.log("this is demo data, fill out onboarding");
+  } else {
+    console.log("this is custom data");
+  }
+
   return (
     <div className="landingWrapper">
       <BarChart />
