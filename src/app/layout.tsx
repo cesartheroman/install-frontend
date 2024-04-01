327206ba25999a5ebbb1f859c3a2a9096f9f238f
Cesar Roman <cesardroman18@gmail.com>
2024-04-01 16:43:45 -0500

    fix: cleaned up layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import { ClerkProvider } from "@clerk/nextjs";
-
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 import { UserButton } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
