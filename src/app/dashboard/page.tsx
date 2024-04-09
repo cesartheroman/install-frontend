9e6dadf71492db6fe3798dc9330050a49b096d87
Cesar Roman <cesardroman18@gmail.com>
2024-04-09 13:05:27 -0500

    style(kwh-cost): restyled and added dummy hardcoded data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -163,11 +163,16 @@ const Dashboard = () => {
           </Card>
           <Card x-chunk="dashboard-01-chunk-5">
             <CardHeader>
-              <CardTitle>Recent Sales</CardTitle>
+              <CardTitle>KwH used</CardTitle>
               <CardDescription>See your electric cost by KwH</CardDescription>
             </CardHeader>
             <CardContent className="grid gap-8">
-              <div className="flex items-center gap-4"></div>
+              <div className="text-2xl font-flex items-center gap-4">
+                $231.89
+              </div>
+              <p className="text-xs text-muted-foreground">
+                +20.1% from last month
+              </p>
             </CardContent>
           </Card>
         </div>
