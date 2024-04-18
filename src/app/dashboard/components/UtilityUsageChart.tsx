a7d617856875e2b0947f70ed91886b6bffaabf7f
Cesar Roman <cesardroman18@gmail.com>
2024-04-18 14:04:06 -0500

    refactor(utility-chart): removed placeholder data

--- src/app/dashboard/components/UtilityUsageChart.tsx
+++ src/app/dashboard/components/UtilityUsageChart.tsx
@@ -12,7 +12,7 @@ const UtilityUsageChart = ({ userData }: { userData: User }) => (
   <Card className="lg:col-span-2" x-chunk="dashboard-01-chunk-4">
     <CardHeader className="flex flex-row items-center">
       <div className="grid gap-2">
-        <CardTitle>Utility Usage by Appliance{userData.zip}</CardTitle>
+        <CardTitle>Utility Usage by Appliance</CardTitle>
         <CardDescription>Recent utility usage.</CardDescription>
       </div>
     </CardHeader>
