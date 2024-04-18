8c604abcfbb6d79a3f4914a4d4011a052278cd63
Cesar Roman <cesardroman18@gmail.com>
2024-04-18 15:06:28 -0500

    feat(cost-comparison): fleshed out information to be shown

--- src/app/dashboard/components/CostComparisonTool.tsx
+++ src/app/dashboard/components/CostComparisonTool.tsx
@@ -10,11 +10,30 @@ const CostComparisonTool = () => (
   <Card x-chunk="dashboard-01-chunk-5">
     <CardHeader>
       <CardTitle>Cost Comparison Tool</CardTitle>
-      <CardDescription>See your electric cost by KwH</CardDescription>
+      <CardDescription>See your Energy Breakdown!</CardDescription>
     </CardHeader>
     <CardContent className="grid gap-8">
-      <div className="text-2xl font-flex items-center gap-4">$231.89</div>
-      <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+      <div className="text-lg font-flex items-center gap-4">
+        <p className="text-md font-flex items-center gap-4">
+          Energy Breakdown by Cost
+        </p>
+        $85 / month average
+        <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+      </div>
+    </CardContent>
+    <CardContent className="grid gap-8">
+      <div className="text-lg font-flex items-center gap-4">
+        <p className="text-md font-flex items-center gap-4">
+          Energy Breakdown by Savings
+        </p>
+        $10 / month average
+        <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+      </div>
+    </CardContent>
+    <CardContent className="grid gap-8">
+      <p className="text-xs text-muted-foreground">
+        Discover Other Savings via Grid Interactive Programs
+      </p>
     </CardContent>
   </Card>
 );
