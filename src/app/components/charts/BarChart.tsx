576f0ff9a93e9ffde1528d705d9fd354cf7fc4cd
Cesar Roman <cesardroman18@gmail.com>
2024-04-18 11:57:08 -0500

    refactor(bar-chart): used userData from props in order to render chart

--- src/app/components/charts/BarChart.tsx
+++ src/app/components/charts/BarChart.tsx
@@ -9,71 +9,61 @@ import {
   Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import { User } from "@/app/interfaces";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-);
-
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top" as const,
-    },
-    title: {
-      display: true,
-      text: "Compare two appliances by month",
-    },
-  },
-};
-
-const labels = [
-  "February 2023",
-  "March 2023",
-  "April 2023",
-  "May 2023",
-  "June 2023",
-  "July 2023",
-  "August 2023",
-  "September 2023",
-  "October 2023",
-  "November 2023",
-  "December 2023",
-  "January 2024",
-];
-
-const applianceAMonthlyCosts = [
-  52.831, 52.655, 94.642, 25.031, 42.981, 25.413, 73.575, 90.008, 92.173,
-  13.962, 47.757, 87.798,
-];
-
-const applianceBMonthlyCosts = [...applianceAMonthlyCosts].reverse();
-
-const data = {
-  labels,
-  datasets: [
-    {
-      label: "Appliance A",
-      // NOTE: think about how this data should be modelled, cost vs months.
-      data: applianceAMonthlyCosts.map((monthlyCost) => monthlyCost),
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Appliance B",
-      data: applianceBMonthlyCosts.map((monthlyCost) => monthlyCost),
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
+import { User, Washer, Dryer } from "@/app/interfaces";
 
 const BarChart = ({ userData }: { userData: User }) => {
-  return <Bar className="chart" options={options} data={data} />;
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  );
+
+  const applianceAInfo: Washer = userData.washer;
+  const applianceADisplayName = `${applianceAInfo.brand_name} ${applianceAInfo.model_name}`;
+
+  const applianceBInfo: Dryer = userData.dryer;
+  const applianceBDisplayName = `${applianceBInfo.brand_name} ${applianceBInfo.model_name}`;
+
+  const applianceACosts = applianceAInfo.electricity_cost;
+  const applianceBCosts = applianceBInfo.gas_cost;
+
+  const labels = applianceAInfo.date_names;
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Compare two appliances by month",
+      },
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: `${applianceADisplayName}`,
+        // NOTE: think about how this data should be modelled, cost vs months.
+        data: applianceACosts.map((monthlyCost) => monthlyCost),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: `${applianceBDisplayName}`,
+        data: applianceBCosts.map((monthlyCost) => monthlyCost),
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  return <Bar options={options} data={data} />;
 };
 
 export default BarChart;
