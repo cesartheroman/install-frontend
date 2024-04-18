03c59913de3bfdd85105479fc58d5155e63c45de
Cesar Roman <cesardroman18@gmail.com>
2024-04-17 22:02:22 -0500

    refactor(utility-usage-chart): introduced usage of props to make components dynamic

--- src/app/dashboard/components/UtilityUsageChart.tsx
+++ src/app/dashboard/components/UtilityUsageChart.tsx
@@ -6,12 +6,13 @@ import {
   CardTitle,
 } from "@/app/components/ui/card";
 import BarChart from "@/app/components/charts/BarChart";
+import { User } from "@/app/interfaces";
 
-const UtilityUsageChart = () => (
+const UtilityUsageChart = ({ userData }: { userData: User }) => (
   <Card className="lg:col-span-2" x-chunk="dashboard-01-chunk-4">
     <CardHeader className="flex flex-row items-center">
       <div className="grid gap-2">
-        <CardTitle>Utility Usage by Appliance</CardTitle>
+        <CardTitle>Utility Usage by Appliance{userData.zip}</CardTitle>
         <CardDescription>Recent utility usage.</CardDescription>
       </div>
     </CardHeader>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import NavBar from "./components/NavBar";
 import UtilityUsageChart from "./components/UtilityUsageChart";
@@ -11,15 +11,18 @@ import { User } from "../interfaces";
 import data from "../../lib/user_data.json";
 
 const Dashboard = () => {
-  const [userData, setUserData] = useState<User>(data);
-  console.log("this is data:", userData);
+  const [userData, setUserData] = useState<User | null>(null);
+
+  useEffect(() => {
+    setUserData(data);
+  }, []);
 
   return (
     <div className="flex min-h-screen w-full flex-col">
       <NavBar />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         <div className="grid gap-4 md:gap-8 lg:grid-cols-3 ">
-          <UtilityUsageChart />
+          {userData && <UtilityUsageChart userData={userData} />}
           <CostComparisonTool />
         </div>
         <div className="grid gap-4 md:gap-8 lg:col-span-2">
