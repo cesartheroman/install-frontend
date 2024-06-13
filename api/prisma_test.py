9b4238e80c32c65033a5ba5099b351d1f4892859
chad-install <chad@joininstall.com>
2024-06-12 21:51:06 -0400

    Created basic database and schema, prisma_test.py adds user

--- api/prisma_test.py
+++ api/prisma_test.py
@@ -1,19 +1,54 @@
 import asyncio
 from prisma import Prisma
+from prisma.models import User
+
 
 async def main() -> None:
-    prisma = Prisma()
-    await prisma.connect()
+    db = Prisma(auto_register=True)
+    await db.connect()
 
     # write your queries here
-    user = await prisma.user.create(
-        data={
-            'name': 'Robert',
-            'email': 'robert@craigie.dev'
-        },
+    # user = await User.prisma().create(
+    #     data={
+    #         'name': 'Erick Salvatierra',
+    #         'email': 'erick@joininstall.com',
+    #         'street': '3325 W Potomac Ave',
+    #         'city': 'Chicago',
+    #         'state': 'IL',
+    #         'zip': 60651,
+    #         'phone': 2247039929
+    #     },
+    # )
+
+
+    user = await db.user.find_unique(
+        where={
+            'email': 'erick@joininstall.com',
+        }
     )
 
-    await prisma.disconnect()
+    # print(user.id)
+    dryer = {
+        'applianceType': 'gas',
+        'applianceModelId': 'XXR-40',
+        # 'owner': user,
+        'ownerId': user.id
+    }
+
+    # adddryer = await db.dryer.create(
+    #     data=dryer
+    # )
+
+    finddryer = await db.dryer.find_unique(
+        where={
+            'ownerId': user.id
+        }
+    )
+
+    print(finddryer.applianceType)
+
+
+    await db.disconnect()
 
 if __name__ == '__main__':
     asyncio.run(main())
